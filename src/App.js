51a9f49bf1bddd8d7b9d6fd090877c2230b1cd99
doseOfMazen <rexnowa@outlook.com>
2019-11-19 00:18:39 -0600

    First day is done.
    I dont' how to make a block of styles and react has been a bit diffrent now. So, I've to relearn somethings.
    
    Now I'm trying to make a bacis hero page i guess...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let username = 'Godpspeed'
+  let headerStyle = {
+    color: '312628',
+    font-size: '30px'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#region -- old code
+/*
+< header className = "App-header" >
+  <
+  img src = {
+    logo
+  }
+className = "App-logo"
+alt = "logo" / >
+  <
+  p >
+  Edit < code > src / App.js < /code> and save. <
+  /p> <
+  a
+className = "App-link"
+href = "https://reactjs.org"
+target = "_blank"
+rel = "noopener noreferrer" >
+  Learn React <
+  /a> <
+  /header>
+  */
+  //#endregion
