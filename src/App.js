d43edddf04dca0c01faee7fe99013e088db19a1d
doseOfMazen <rexnowa@outlook.com>
2019-11-21 00:43:48 -0600

    finished the skeleton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  let username = 'Godpspeed'
-  // let headerStyle = {
-  //   'color': '312628',
-  //   'font-size:' '30px'
-  // }
+function Aggregate() {
   return (
-    <div className="App">
-      <h1>{username}</h1>
+    <div style = {{width: "40%", display: "inline-block"}}>
+      <h2>Number Text</h2>
+    </div>
+  );
+}
+
+function Filter() {
+  return (
+    <div>
+      <img alt = ""/>
+      <input type="text" placeholder="type some text"/>
+    </div>
+  );
+}
+
+function Playlist() {
+  return (
+    <div style = {
+      {
+        width: "25%",
+        display: "inline-block",
+        fontSize: "20px"
+      }
+    }>
+      <img  alt=""/>
+      <h3>Playlist Name</h3>
+      <ul style = {{"listStyle": "none"}}>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+        <li>Song 4</li>
+      </ul>
+    </div>
+  );
+}
+
+function App() {
+  //let username = 'Godpspeed'
+  return (
+    <div className = "App">
+      <h1 style = {{"font-size": "55px"}}>Title</h1>
+      {Aggregate()}
+      {Aggregate()}
+      {Filter()}
+      {Playlist()}
+      {Playlist()}
+      {Playlist()}
+      {Playlist()}
     </div>
   );
 }
