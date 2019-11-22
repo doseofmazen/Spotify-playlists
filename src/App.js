61dcf442ba5751539ffa0a04dd4f9449956d39d1
doseOfMazen <rexnowa@outlook.com>
2019-11-21 18:19:01 -0600

    Made up fake data
    Made fakeData object to be used intead of placeholders. This object will act as a placeholder for real data from server ones that's configured.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+//fake data that will replaced by server data
+let fakeData = {
+  user:
+  {
+    name: '🔥Godspeed',
+    playlist: [{
+      name: 'CHH🔥',
+      song: ['Watch this', 'Co$st', 'Bag', 'Thass God']
+    }, {
+      name: 'Car',
+      song: ['PANORAMA', 'Bros', 'Swish', 'One Day(Feat. Beleaf)']
+    }, {
+      name: 'Workout',
+      song: ['Wow', 'Going Bad(Feat. Drake)', 'Project Dreams', 'Coming In Hot']
+    }, {
+      name: 'Home',
+      song: ['Lala', 'Nana', 'Mama', 'Jojo']
+    }]
+  }
+};
+
+//Playlist aggregate
 function Aggregate() {
   return (
     <div style = {{width: "40%", display: "inline-block"}}>
@@ -10,6 +32,7 @@ function Aggregate() {
   );
 }
 
+//search box to be used for filtering songs
 function Filter() {
   return (
     <div>
@@ -19,6 +42,7 @@ function Filter() {
   );
 }
 
+//the actual playlist
 function Playlist() {
   return (
     <div style = {
@@ -39,21 +63,42 @@ function Playlist() {
     </div>
   );
 }
+//main function so inti the app
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
 
-function App() {
-  //let username = 'Godpspeed'
-  return (
-    <div className = "App">
-      <h1 style = {{"font-size": "55px"}}>Insert title</h1>
-      {Aggregate()}
-      {Aggregate()}
-      {Filter()}
-      {Playlist()}
-      {Playlist()}
-      {Playlist()}
-      {Playlist()}
+  componentDidMount() {
+    this.setState({serverData: fakeData})
+  }
+
+  render() {
+    return (
+       <div className = "App">
+        <h1 style = {{"font-size": "55px"}}>
+          {
+            this.state.serverData.user &&
+            this.state.serverData.user.name
+          }'s playlist
+        </h1>
+
+
+
+
+
+        {/* Calling funtions */}
+        {Aggregate()}
+        {Aggregate()}
+        {Filter()}
+        {Playlist()}
+        {Playlist()}
+        {Playlist()}
+        {Playlist()}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
